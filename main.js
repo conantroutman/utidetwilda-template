92e4f49510399fe59951e3208c414d5dd686fdab
conantroutman <johsod96@gmail.com>
2021-03-22 00:40:55 +0100

    Implemented mobile navigation

--- main.js
+++ main.js
@@ -1,3 +1,34 @@
 const rellax = new Rellax('.rellax');
 const scroll = new SmoothScroll('a[href*="#"]');
 //scrollReveal().reveal('.reveal');
+
+const hamburgerButton = document.querySelector('.burger');
+const mobileNav = document.querySelector('nav');
+const navLinks = document.querySelectorAll('.nav-link');
+
+// EVENT LISTENERS
+
+hamburgerButton.addEventListener("click", toggleMobileNav);
+navLinks.forEach(link => {
+    link.addEventListener('click', closeMobileNav);
+});
+
+// FUNCTIONS
+
+function toggleMobileNav() {
+    if (mobileNav.classList.contains('open')) {
+        closeMobileNav();
+    } else {
+        openMobileNav();
+    }
+}
+
+function openMobileNav() {
+    mobileNav.classList.add('open');
+    hamburgerButton.classList.add('is-active');
+}
+
+function closeMobileNav() {
+    mobileNav.classList.remove('open');
+    hamburgerButton.classList.remove('is-active');
+}
