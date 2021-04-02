8194c0e1197d2b16a837f0658f931e7855ff40bc
conantroutman <johsod96@gmail.com>
2021-04-02 17:36:56 +0200

    Mostly finished

--- index.js
+++
@@ -1,5 +0,0 @@
-if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
-    document.querySelector('.mobile-only').style.display = 'block';
-}else{
-    document.querySelector('.mobile-only').style.display = 'none';
-}
